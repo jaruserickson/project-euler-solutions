3298a2b63c20241016238b405273a69111effc6d
Jacob Erickson <jacob.erickson@mail.utoronto.ca>
2017-02-02 16:32:46 -0500

    cleaned up with catch + eratosthenes sieve

--- python/003.py
+++ python/003.py
@@ -2,7 +2,7 @@ def largest_pf(n):
 	#runs in O(nlgn) time (divide and conquer optimization)
 	x, largest = 2, 1
 	while x ** 2 < n: 
-	    #this runs in this order so is_prime(x) will only run when n % x == 0
+		# runs as many times as can be divided by the prime.
 		while n % x == 0:
 			if x > largest:
 				largest = x
@@ -16,3 +16,4 @@ if __name__ == "__main__":
 	print(largest_pf(8))
 	print(largest_pf(5))
 	print(largest_pf(24))
+	#print(largest_pf(961748941))
