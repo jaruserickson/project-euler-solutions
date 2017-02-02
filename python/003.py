5384b3feab243f7bde64134615f80d175079f950
Jacob Erickson <jacob.erickson@mail.utoronto.ca>
2017-02-02 15:38:48 -0500

    updated with fundemental theorem of arith + largest variable

--- python/003.py
+++ python/003.py
@@ -12,14 +12,19 @@ def is_prime(n):
 
 def largest_pf(n):
 	#runs in O(nlgn) time (divide and conquer optimization)
-	x = 2
-	while x < n: 
+	x, largest = 2, 1
+	while x ** 2 < n: 
 	    #this runs in this order so is_prime(x) will only run when n % x == 0
-		if n % x == 0 and is_prime(x):
+		while n % x == 0 and is_prime(x):
+			if x > largest:
+				largest = x
 			n //= x 
 		x += 1
-	return n
+	return largest if largest > n else n
 
 
 if __name__ == "__main__":
 	print(largest_pf(600851475143))
+	print(largest_pf(8))
+	print(largest_pf(5))
+	print(largest_pf(24))
