dc40dc17844eccf232050a34e8537120875b32a0
Jason Piao <jason.piao@mail.utoronto.ca>
2017-02-12 04:04:03 -0500

    brute force algorithm

---
+++ python/004.py
@@ -0,0 +1,20 @@
+def largest_palindrome():
+    lst = []
+    for number in range(100,1000):
+        for num in range(100,1000):
+            #cast as string to make it easier to check if its a palindrome
+            if is_palindrome(str(number * num)):
+                lst.append(number * num)
+    return max(lst)
+
+#checks if n is a palindrome
+def is_palindrome(n):
+    new = ""
+    for i in range(len(n)-1, -1, -1):
+        new += n[i]
+    if new == n:
+        return True
+    return False
+
+
+print(largest_palindrome())
